925866543befb91fc03542617a8925877f91ad43
Afrizaaa017 <afrizaharapan@gmail.com>
2024-08-22 20:30:07 +0700

    hasil dari praktikum1 PBO

---
+++ Soal2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author Afriza
+ */
+public class Soal2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        int i = 43;
+        String s = (i<40)?"life":(i>50)?"universe":"eveyrthing";
+        System.out.println(s);
+    }
+}
---
+++ welcome.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author Afriza
+ */
+public class welcome {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        String greeting = "Welcome to Core Java!"; 
+        System.out.println(greeting);
+        for (int i=0; i< greeting.length(); i++) 
+            System.out.print("="); 
+        System.out.println();
+    }
+    
+}
